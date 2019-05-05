01501eefa149a50b54039f0fe08df1844cb398ef
Reyhan Alphard Savero <newrey9227@gmail.com>
2019-05-05 23:09:41 +0700

    First Commit

---
+++ Assets/Scripts/PushController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PushController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+    }
+
+    public void Push()
+    {
+        player.transform.Translate(Vector2.right * 3 * Time.deltaTime);
+    }
+}
