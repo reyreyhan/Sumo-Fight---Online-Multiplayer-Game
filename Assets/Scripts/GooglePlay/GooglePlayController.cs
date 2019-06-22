7091b5c34707b9e63a81aef1611dd76ddd3bc142
Reyhan Alphard Savero <newrey9227@gmail.com>
2019-06-23 00:49:22 +0700

    Update | Player Asset

---
+++ Assets/Scripts/GooglePlay/GooglePlayController.cs
@@ -0,0 +1,42 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooglePlayController : MonoBehaviour
+{
+    public GameObject loginBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Auth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Auth()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        Social.localUser.Authenticate((bool success) =>
+        {
+            if (success == true)
+            {
+                Debug.Log("Brhsl");
+            }
+            else
+            {
+                Debug.Log("Gagal Login");
+                loginBtn.SetActive(true);
+            }
+        });
+    }
+}
